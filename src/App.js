0a71d2eea1137747ad6bed3c11768a5eb1a96ee7
shimoooooooo <shosha.mohamed27@gmail.com>
2024-02-29 00:58:52 +0200

    create project

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import { useRef, useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [x , setx] = useState([])
+
+  const inputRef = useRef ()
+
+  const add = () => {
+    const value = inputRef.current.value
+
+    const newData = {completed : false ,value}
+
+    setx([...x , newData])
+
+     inputRef.current.value = ""
+  }
+  const itemDone = (index) => {
+    const newx = [...x]
+    x[index].completed = !x[index].completed 
+    setx(newx)
+  }
+
+  const toDelete = (index) => {
+    const newx = [...x]
+    newx.splice(index , 1)
+    setx(newx)
+
+  }
+  return (
+    <div className="App">
+
+      
+      <h2>To Do List</h2>
+
+      <ul>
+        {
+           x.map(({value ,completed }, index)=> {
+            return <div className='div-2'><li className={completed ? "diffstyle" : ""} onClick={ () => itemDone(index)}>{value}</li>
+
+            <span onClick={()=> toDelete(index)}>❌</span>
+            </div>
+           })
+        }
+      </ul>
+      <input ref={inputRef} placeholder='Enter new task...'/>
+      <button onClick={add}>Add</button>
+    </div>
+  );
+}
+
+export default App;
